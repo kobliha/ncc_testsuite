9ff72aee1f905b9b4b4e44ba05db2061cac6dfe2
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-06-13 08:49:32 +0000

    All repos using one function to get to the data

--- lib/ncc_testsuite/repository.rb
+++ lib/ncc_testsuite/repository.rb
@@ -3,17 +3,23 @@ class NccTestsuite::Repository
 
   # Lists all currently known repositories (aliases)
   def self.all
-    $config['Repos'].keys.sort
+    all_repos.keys.sort
   end
 
   # returns URL for a repository alias
   def self.url repo_alias
-    url = $config['Repos'][repo_alias]
+    url = all_repos[repo_alias]
 
     if url == nil || url == ""
       raise "Repository URL for #{repo_alias} is not defined"
     end
 
     url
+  end
+
+  private
+
+  def self.all_repos
+    $config['Repos']
   end
 end
